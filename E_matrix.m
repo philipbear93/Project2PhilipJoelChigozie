71f9c5f91fe791d41962782eb327696fa9ddd85f
philipbear93 <philipbear93@yahoo.com>
2016-11-06 10:53:59 -0500

    Debugged E and N
    E_brick and N_brick had bugs due to a strange matlab indexing issue.
    This update resolves those issues.

--- E_matrix.m
+++ E_matrix.m
@@ -6,8 +6,12 @@ val2=1+nu;
 val3=1-2*nu;
 Emat=zeros(6,6); % Creates a matrix of all zeros
 Emat(1:3,1:3)=nu; % Fills the top left 3x3 block with nu
-Emat([1,2,3],[1,2,3])=val1; % Overwrites the diagonal
-Emat([4,5,6],[4,5,6])=val3/2; % Writes the bottom right diagonal
+Emat(1,1)=val1; % Overwrites the diagonal
+Emat(2,2)=val1;
+Emat(3,3)=val1;
+Emat(4,4)=val3/2; % Writes the bottom right diagonal
+Emat(5,5)=val3/2; 
+Emat(6,6)=val3/2;
 Emat=Emat*E/(val2*val3); % Multiplies by the outside constants
 
 end
--- N_brick.m
+++ N_brick.m
@@ -8,9 +8,9 @@ EtaI=[-1 -1 1 1 -1 -1 1 1];
 ZetaI=[1 -1 -1 1 1 -1 -1 1];
 N=[]; % creates N matrix
 for i=1:8 % iterates over each node
-    Ni(i)=(1/8)*(1+Epsilon*EpsilonI(i))*(1+Eta*EtaI(i))*(1+Zeta*ZetaI(i)); % Calculates each nodal shape function
-    Ne=zeros(3,3); % Creates a nodal N matrix
-    Ne([1 2 3],[1 2 3])=Ni(i); % stores the shape function on the diagonal
+    Ni=(1/8)*(1+Epsilon*EpsilonI(i))*(1+Eta*EtaI(i))*(1+Zeta*ZetaI(i)); % Calculates each nodal shape function
+    Ne=[Ni Ni Ni]; % Creates a nodal N matrix
+    Ne=diag(Ne); % stores the shape function on the diagonal
     N=[N Ne]; % appends the element shape function matrix with the nodal matrix
 end
 
