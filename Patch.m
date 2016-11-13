250b56520d694de42bd26785e9aa2d52cf8775e4
JSummerfield17 <summerfield.2@wright.edu>
2016-11-13 16:10:28 -0500

    Debugged Patch
    Patch now divides unifrom brick element into eight nonuniform bricks.
    Next step is to apply forces and determine stresses and strains for
    comparisons at corners.

--- Patch.m
+++ Patch.m
@@ -5,12 +5,12 @@ function out = Patch( xvec, yvec, zvec )
 %   each new element is compared. If the strain is uniform among the
 %   corners and also among elements, the patch test is passed. 
 
-xrange = max(xvec)-min(xvec);
-yrange = max(yvec)-min(yvec);
-zrange = max(zvec)-min(zvec);
-xvec = xvec'
-yvec = yvec'
-zvec = zvec'
+xavg = mean(xvec);
+yavg = mean(yvec);
+zavg = mean(zvec);
+xvec = xvec';
+yvec = yvec';
+zvec = zvec';
 
 node1 = [xvec(1) yvec(1) zvec(1)];
 node2 = [xvec(2) yvec(2) zvec(2)];
@@ -21,7 +21,11 @@ node6 = [xvec(6) yvec(6) zvec(6)];
 node7 = [xvec(7) yvec(7) zvec(7)];
 node8 = [xvec(8) yvec(8) zvec(8)];
 
-PatchPt = [(1/3*xrange), (0.5*yrange), (1/3*zrange)];
+if yavg == 0
+    PatchPt = [xavg yavg+1/3 zavg];
+else
+    PatchPt = [xavg yavg*1/3 zavg];
+end
 
 Bi1 = [(node1(1)+node2(1))/2, (node1(2)+node2(2))/2, (node1(3)+node2(3))/2];
 Bi2 = [(node2(1)+node3(1))/2, (node2(2)+node3(2))/2, (node2(3)+node3(3))/2];
@@ -30,7 +34,7 @@ Bi4 = [(node1(1)+node4(1))/2, (node1(2)+node4(2))/2, (node1(3)+node4(3))/2];
 Bi5 = [(node5(1)+node6(1))/2, (node5(2)+node6(2))/2, (node5(3)+node6(3))/2];
 Bi6 = [(node6(1)+node7(1))/2, (node6(2)+node7(2))/2, (node6(3)+node7(3))/2];
 Bi7 = [(node7(1)+node8(1))/2, (node7(2)+node8(2))/2, (node7(3)+node8(3))/2];
-Bi8 = [(node1(1)+node8(1))/2, (node1(2)+node8(2))/2, (node1(3)+node8(3))/2];
+Bi8 = [(node5(1)+node8(1))/2, (node5(2)+node8(2))/2, (node1(3)+node8(3))/2];
 Bi9 = [(node1(1)+node5(1))/2, (node1(2)+node5(2))/2, (node1(3)+node5(3))/2];
 Bi10 = [(node2(1)+node6(1))/2, (node2(2)+node6(2))/2, (node2(3)+node6(3))/2];
 Bi11 = [(node3(1)+node7(1))/2, (node3(2)+node7(2))/2, (node3(3)+node7(3))/2];
@@ -42,10 +46,10 @@ Bi16 = [(Bi2(1)+Bi6(1))/2, (Bi2(2)+Bi6(2))/2, (Bi2(3)+Bi6(3))/2];
 Bi17 = [(Bi3(1)+Bi7(1))/2, (Bi3(2)+Bi7(2))/2, (Bi3(3)+Bi7(3))/2];
 Bi18 = [(Bi4(1)+Bi8(1))/2, (Bi4(2)+Bi8(2))/2, (Bi4(3)+Bi8(3))/2];
 
-numlines = 0
+numlines = 0;
 
-%Element 1: Node1, Bi1, Bi13, Bi4, Bi9, Bi15, PatchPt, Bi18
-Elem1 = [node1; Bi1; Bi13; Bi4; Bi9; Bi15; PatchPt; Bi18]
+%Element 1: node1, Bi1, Bi13, Bi4, Bi9, Bi15, PatchPt, Bi18
+Elem1 = [node1; Bi1; Bi13; Bi4; Bi9; Bi15; PatchPt; Bi18];
 E1xvec = Elem1(:,1);
 E1yvec = Elem1(:,2);
 E1zvec = Elem1(:,3);
@@ -69,7 +73,7 @@ E3zvec = Elem3(:,3);
 
 
 %Element 4: Bi15, Bi10, Bi16, PatchPt, Bi5, node6, Bi6, Bi14 
-Elem4 = [Bi15; Bi1; Bi16; PatchPt; Bi5; node6; Bi6; Bi14];
+Elem4 = [Bi15; Bi10; Bi16; PatchPt; Bi5; node6; Bi6; Bi14];
 E4xvec = Elem4(:,1);
 E4yvec = Elem4(:,2);
 E4zvec = Elem4(:,3);
@@ -100,8 +104,8 @@ E7zvec = Elem7(:,3);
 
 
 
-%Element 8:  PatchPt; Bi16; Bi11; Bi17; Bi14; Bi6; Bi7; Bi7
-Elem8 = [PatchPt; Bi16; Bi11; Bi17; Bi14; Bi6; Bi7; Bi7];
+%Element 8:  PatchPt; Bi16; Bi11; Bi17; Bi14; Bi6; node7; Bi7
+Elem8 = [PatchPt; Bi16; Bi11; Bi17; Bi14; Bi6; node7; Bi7];
 E8xvec = Elem8(:,1);
 E8yvec = Elem8(:,2);
 E8zvec = Elem8(:,3);
@@ -116,104 +120,105 @@ lines(numlines+5,:)=[Elem1(5,:) Elem1(6,:)];
 lines(numlines+6,:)=[Elem1(6,:) Elem1(7,:)];
 lines(numlines+7,:)=[Elem1(7,:) Elem1(8,:)];
 lines(numlines+8,:)=[Elem1(8,:) Elem1(5,:)];
-% lines(numlines+9,:)=[Bi18 Bi9];
-% lines(numlines+10,:)=[Bi1 Bi15];
-% lines(numlines+11,:)=[Bi13 PatchPt];
-% lines(numlines+12,:)=[Bi4 Bi18];
-% lines(numlines+13,:)=[Bi9 Bi15];
-% lines(numlines+14,:)=[Bi15 PatchPt];
-% lines(numlines+15,:)=[PatchPt Bi18];
-% lines(numlines+16,:)=[Bi18 Bi9];
-% lines(numlines+17,:)=[Bi9 node5];
-% lines(numlines+18,:)=[node5 Bi5];
-% lines(numlines+19,:)=[Bi5 Bi14];
-% lines(numlines+20,:)=[Bi14 Bi8];
-% lines(numlines+21,:)=[Bi8 node5];
-% lines(numlines+22,:)=[Bi15 Bi5];
-% lines(numlines+23,:)=[Bi14 PatchPt];
-% lines(numlines+24,:)=[Bi18 Bi8];
-% lines(numlines+25,:)=[Elem3(1,:) Elem3(2,:)];
-% lines(numlines+26,:)=[Elem3(2,:) Elem3(3,:)];
-% lines(numlines+27,:)=[Elem3(3,:) Elem3(4,:)];
-% lines(numlines+28,:)=[Elem3(4,:) Elem3(1,:)];
-% lines(numlines+29,:)=[Elem3(5,:) Elem3(6,:)];
-% lines(numlines+30,:)=[Elem3(6,:) Elem3(7,:)];
-% lines(numlines+31,:)=[Elem3(7,:) Elem3(8,:)];
-% lines(numlines+32,:)=[Elem3(8,:) Elem3(5,:)];
-% lines(numlines+33,:)=[Elem3(1,:) Elem3(5,:)];
-% lines(numlines+34,:)=[Elem3(2,:) Elem3(6,:)];
-% lines(numlines+35,:)=[Elem3(3,:) Elem3(7,:)];
-% lines(numlines+36,:)=[Elem3(4,:) Elem3(8,:)];
-% lines(numlines+37,:)=[Elem4(1,:) Elem4(2,:)];
-% lines(numlines+38,:)=[Elem4(2,:) Elem4(3,:)];
-% lines(numlines+39,:)=[Elem4(3,:) Elem4(4,:)];
-% lines(numlines+40,:)=[Elem4(4,:) Elem4(1,:)];
-% lines(numlines+41,:)=[Elem4(5,:) Elem4(6,:)];
-% lines(numlines+42,:)=[Elem4(6,:) Elem4(7,:)];
-% lines(numlines+43,:)=[Elem4(7,:) Elem4(8,:)];
-% lines(numlines+44,:)=[Elem4(8,:) Elem4(5,:)];
-% lines(numlines+45,:)=[Elem4(1,:) Elem4(5,:)];
-% lines(numlines+46,:)=[Elem4(2,:) Elem4(6,:)];
-% lines(numlines+47,:)=[Elem4(3,:) Elem4(7,:)];
-% lines(numlines+48,:)=[Elem4(4,:) Elem4(8,:)];
-% lines(numlines+49,:)=[Elem5(1,:) Elem5(2,:)];
-% lines(numlines+50,:)=[Elem5(2,:) Elem5(3,:)];
-% lines(numlines+51,:)=[Elem5(3,:) Elem5(4,:)];
-% lines(numlines+52,:)=[Elem5(4,:) Elem5(1,:)];
-% lines(numlines+53,:)=[Elem5(5,:) Elem5(6,:)];
-% lines(numlines+54,:)=[Elem5(6,:) Elem5(7,:)];
-% lines(numlines+55,:)=[Elem5(7,:) Elem5(8,:)];
-% lines(numlines+56,:)=[Elem5(8,:) Elem5(5,:)];
-% lines(numlines+57,:)=[Elem5(1,:) Elem5(5,:)];
-% lines(numlines+58,:)=[Elem5(2,:) Elem5(6,:)];
-% lines(numlines+59,:)=[Elem5(3,:) Elem5(7,:)];
-% lines(numlines+60,:)=[Elem5(4,:) Elem5(8,:)];
-% lines(numlines+61,:)=[Elem6(1,:) Elem6(2,:)];
-% lines(numlines+62,:)=[Elem6(2,:) Elem6(3,:)];
-% lines(numlines+63,:)=[Elem6(3,:) Elem6(4,:)];
-% lines(numlines+64,:)=[Elem6(4,:) Elem6(1,:)];
-% lines(numlines+65,:)=[Elem6(5,:) Elem6(6,:)];
-% lines(numlines+66,:)=[Elem6(6,:) Elem6(7,:)];
-% lines(numlines+67,:)=[Elem6(7,:) Elem6(8,:)];
-% lines(numlines+68,:)=[Elem6(8,:) Elem6(5,:)];
-% lines(numlines+69,:)=[Elem6(1,:) Elem6(5,:)];
-% lines(numlines+70,:)=[Elem6(2,:) Elem6(6,:)];
-% lines(numlines+71,:)=[Elem6(3,:) Elem6(7,:)];
-% lines(numlines+72,:)=[Elem6(4,:) Elem6(8,:)];
-% lines(numlines+73,:)=[Elem7(1,:) Elem7(2,:)];
-% lines(numlines+74,:)=[Elem7(2,:) Elem7(3,:)];
-% lines(numlines+75,:)=[Elem7(3,:) Elem7(4,:)];
-% lines(numlines+76,:)=[Elem7(4,:) Elem7(1,:)];
-% lines(numlines+77,:)=[Elem7(5,:) Elem7(6,:)];
-% lines(numlines+78,:)=[Elem7(6,:) Elem7(7,:)];
-% lines(numlines+79,:)=[Elem7(7,:) Elem7(8,:)];
-% lines(numlines+80,:)=[Elem7(8,:) Elem7(5,:)];
-% lines(numlines+81,:)=[Elem7(1,:) Elem7(5,:)];
-% lines(numlines+82,:)=[Elem7(2,:) Elem7(6,:)];
-% lines(numlines+83,:)=[Elem7(3,:) Elem7(7,:)];
-% lines(numlines+84,:)=[Elem7(4,:) Elem7(8,:)];
-% lines(numlines+73,:)=[Elem8(1,:) Elem8(2,:)];
-% lines(numlines+74,:)=[Elem8(2,:) Elem8(3,:)];
-% lines(numlines+75,:)=[Elem8(3,:) Elem8(4,:)];
-% lines(numlines+76,:)=[Elem8(4,:) Elem8(1,:)];
-% lines(numlines+77,:)=[Elem8(5,:) Elem8(6,:)];
-% lines(numlines+78,:)=[Elem8(6,:) Elem8(7,:)];
-% lines(numlines+79,:)=[Elem8(7,:) Elem8(8,:)];
-% lines(numlines+80,:)=[Elem8(8,:) Elem8(5,:)];
-% lines(numlines+81,:)=[Elem8(1,:) Elem8(5,:)];
-% lines(numlines+82,:)=[Elem8(2,:) Elem8(6,:)];
-% lines(numlines+83,:)=[Elem8(3,:) Elem8(7,:)];
-% lines(numlines+84,:)=[Elem8(4,:) Elem8(8,:)]
+lines(numlines+9,:)=[Elem1(1,:) Elem1(5,:)];
+lines(numlines+10,:)=[Elem1(2,:) Elem1(6,:)];
+lines(numlines+11,:)=[Elem1(3,:) Elem1(7,:)];
+lines(numlines+12,:)=[Elem1(4,:) Elem1(8,:)];
+lines(numlines+13,:)=[Elem2(1,:) Elem2(2,:)];
+lines(numlines+14,:)=[Elem2(2,:) Elem2(3,:)];
+lines(numlines+15,:)=[Elem2(3,:) Elem2(4,:)];
+lines(numlines+16,:)=[Elem2(4,:) Elem2(1,:)];
+lines(numlines+17,:)=[Elem2(5,:) Elem2(6,:)];
+lines(numlines+18,:)=[Elem2(6,:) Elem2(7,:)];
+lines(numlines+19,:)=[Elem2(7,:) Elem2(8,:)];
+lines(numlines+20,:)=[Elem2(8,:) Elem2(5,:)];
+lines(numlines+21,:)=[Elem2(1,:) Elem2(5,:)];
+lines(numlines+22,:)=[Elem2(2,:) Elem2(6,:)];
+lines(numlines+23,:)=[Elem2(3,:) Elem2(7,:)];
+lines(numlines+24,:)=[Elem2(4,:) Elem2(8,:)];
+lines(numlines+25,:)=[Elem3(1,:) Elem3(2,:)];
+lines(numlines+26,:)=[Elem3(2,:) Elem3(3,:)];
+lines(numlines+27,:)=[Elem3(3,:) Elem3(4,:)];
+lines(numlines+28,:)=[Elem3(4,:) Elem3(1,:)];
+lines(numlines+29,:)=[Elem3(5,:) Elem3(6,:)];
+lines(numlines+30,:)=[Elem3(6,:) Elem3(7,:)];
+lines(numlines+31,:)=[Elem3(7,:) Elem3(8,:)];
+lines(numlines+32,:)=[Elem3(8,:) Elem3(5,:)];
+lines(numlines+33,:)=[Elem3(1,:) Elem3(5,:)];
+lines(numlines+34,:)=[Elem3(2,:) Elem3(6,:)];
+lines(numlines+35,:)=[Elem3(3,:) Elem3(7,:)];
+lines(numlines+36,:)=[Elem3(4,:) Elem3(8,:)];
+lines(numlines+37,:)=[Elem4(1,:) Elem4(2,:)];
+lines(numlines+38,:)=[Elem4(2,:) Elem4(3,:)];
+lines(numlines+39,:)=[Elem4(3,:) Elem4(4,:)];
+lines(numlines+40,:)=[Elem4(4,:) Elem4(1,:)];
+lines(numlines+41,:)=[Elem4(5,:) Elem4(6,:)];
+lines(numlines+42,:)=[Elem4(6,:) Elem4(7,:)];
+lines(numlines+43,:)=[Elem4(7,:) Elem4(8,:)];
+lines(numlines+44,:)=[Elem4(8,:) Elem4(5,:)];
+lines(numlines+45,:)=[Elem4(1,:) Elem4(5,:)];
+lines(numlines+46,:)=[Elem4(2,:) Elem4(6,:)];
+lines(numlines+47,:)=[Elem4(3,:) Elem4(7,:)];
+lines(numlines+48,:)=[Elem4(4,:) Elem4(8,:)];
+lines(numlines+49,:)=[Elem5(1,:) Elem5(2,:)];
+lines(numlines+50,:)=[Elem5(2,:) Elem5(3,:)];
+lines(numlines+51,:)=[Elem5(3,:) Elem5(4,:)];
+lines(numlines+52,:)=[Elem5(4,:) Elem5(1,:)];
+lines(numlines+53,:)=[Elem5(5,:) Elem5(6,:)];
+lines(numlines+54,:)=[Elem5(6,:) Elem5(7,:)];
+lines(numlines+55,:)=[Elem5(7,:) Elem5(8,:)];
+lines(numlines+56,:)=[Elem5(8,:) Elem5(5,:)];
+lines(numlines+57,:)=[Elem5(1,:) Elem5(5,:)];
+lines(numlines+58,:)=[Elem5(2,:) Elem5(6,:)];
+lines(numlines+59,:)=[Elem5(3,:) Elem5(7,:)];
+lines(numlines+60,:)=[Elem5(4,:) Elem5(8,:)];
+lines(numlines+61,:)=[Elem6(1,:) Elem6(2,:)];
+lines(numlines+62,:)=[Elem6(2,:) Elem6(3,:)];
+lines(numlines+63,:)=[Elem6(3,:) Elem6(4,:)];
+lines(numlines+64,:)=[Elem6(4,:) Elem6(1,:)];
+lines(numlines+65,:)=[Elem6(5,:) Elem6(6,:)];
+lines(numlines+66,:)=[Elem6(6,:) Elem6(7,:)];
+lines(numlines+67,:)=[Elem6(7,:) Elem6(8,:)];
+lines(numlines+68,:)=[Elem6(8,:) Elem6(5,:)];
+lines(numlines+69,:)=[Elem6(1,:) Elem6(5,:)];
+lines(numlines+70,:)=[Elem6(2,:) Elem6(6,:)];
+lines(numlines+71,:)=[Elem6(3,:) Elem6(7,:)];
+lines(numlines+72,:)=[Elem6(4,:) Elem6(8,:)];
+lines(numlines+73,:)=[Elem7(1,:) Elem7(2,:)];
+lines(numlines+74,:)=[Elem7(2,:) Elem7(3,:)];
+lines(numlines+75,:)=[Elem7(3,:) Elem7(4,:)];
+lines(numlines+76,:)=[Elem7(4,:) Elem7(1,:)];
+lines(numlines+77,:)=[Elem7(5,:) Elem7(6,:)];
+lines(numlines+78,:)=[Elem7(6,:) Elem7(7,:)];
+lines(numlines+79,:)=[Elem7(7,:) Elem7(8,:)];
+lines(numlines+80,:)=[Elem7(8,:) Elem7(5,:)];
+lines(numlines+81,:)=[Elem7(1,:) Elem7(5,:)];
+lines(numlines+82,:)=[Elem7(2,:) Elem7(6,:)];
+lines(numlines+83,:)=[Elem7(3,:) Elem7(7,:)];
+lines(numlines+84,:)=[Elem7(4,:) Elem7(8,:)];
+lines(numlines+85,:)=[Elem8(1,:) Elem8(2,:)];
+lines(numlines+86,:)=[Elem8(2,:) Elem8(3,:)];
+lines(numlines+87,:)=[Elem8(3,:) Elem8(4,:)];
+lines(numlines+88,:)=[Elem8(4,:) Elem8(1,:)];
+lines(numlines+89,:)=[Elem8(5,:) Elem8(6,:)];
+lines(numlines+90,:)=[Elem8(6,:) Elem8(7,:)];
+lines(numlines+91,:)=[Elem8(7,:) Elem8(8,:)];
+lines(numlines+92,:)=[Elem8(8,:) Elem8(5,:)];
+lines(numlines+93,:)=[Elem8(1,:) Elem8(5,:)];
+lines(numlines+94,:)=[Elem8(2,:) Elem8(6,:)];
+lines(numlines+95,:)=[Elem8(3,:) Elem8(7,:)];
+lines(numlines+96,:)=[Elem8(4,:) Elem8(8,:)];
 
 
-plotline = [lines(1,1:3); lines(1,4:6)];
-plot3(plotline(:,1), plotline(:,2), plotline(:,3));
+% plotline = [lines(1,1:3); lines(1,4:6)];
+% plot3(plotline(:,1), plotline(:,2), plotline(:,3));
 
 
 hold on
-for i = 1:84
-    plotline = [lines(i,1:3); lines(i,4:6)]
+for i = 1:length(lines)
+    plotline = [lines(i,1:3); lines(i,4:6)];
     plot3(plotline(:,1), plotline(:,2), plotline(:,3))
+    axis([-1 1 -1 1 -1 1]);
 end
 
 
